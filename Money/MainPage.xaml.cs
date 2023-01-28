70a27e903f89ce8afe45e934fce8aefd04c73ad2
and078 <andreynovikov078@gmail.com>
2023-01-28 17:11:04 +0200

    add events

--- Money/MainPage.xaml.cs
+++ Money/MainPage.xaml.cs
@@ -21,17 +21,41 @@ namespace Money
             DeviceDisplay.KeepScreenOn = true;
             this.BackgroundColor = Color.Black;
             _ = InitializeCurrencies();
-            _ = DrawChart();
+            _ = DrawChart(mdl);
+
+            mdl.TextChanged += Mdl_TextChanged;
+            mdl.Focused += Mdl_Focused;
+
+            usd.TextChanged += Usd_TextChanged;
+            usd.Focused += Usd_Focused;
+
+            ron.TextChanged += Ron_TextChanged;
+            ron.Focused += Ron_Focused;
+
+            rub.TextChanged += Rub_TextChanged;
+            rub.Focused += Rub_Focused;
+
+            uah.TextChanged += Uah_TextChanged;
+            uah.Focused += Uah_Focused;
+
+            gbp.TextChanged += Gbp_TextChanged;
+            gbp.Focused += Gbp_Focused;
+
+            eur.TextChanged += Eur_TextChanged;
+            eur.Focused += Eur_Focused;
         }
 
-        private async Task DrawChart()
+        private async Task DrawChart(Entry entry)
         {
             await Task.Delay(1000);
-            var entries = new List<ChartEntry>();
+            var charEntries = new List<ChartEntry>();
+            var day = DateTime.Now.DayOfWeek;
+            var currentCurrency = entry.ToString();
+            await DisplayAlert("Changed", currentCurrency.ToString(), "Cancel");
+
             foreach (var currency in currencies)
             {
-                var day = DateTime.Now.DayOfWeek;
-                entries.Add(new ChartEntry((float)currency.Value[0][1])
+                charEntries.Add(new ChartEntry((float)currency.Value[0][1])
                 {
                     Label = day.ToString(),
                     ValueLabel = currency.Value[0][1].ToString(),
@@ -43,7 +67,7 @@ namespace Money
 
             var chart = new LineChart
             {
-                Entries = entries,
+                Entries = charEntries,
                 BackgroundColor = SKColor.Parse("#031b29"),
                 LabelTextSize = 30
             };
@@ -57,20 +81,78 @@ namespace Money
             apiFetcher = new ApiFetcher();
             await Task.Run(async () =>
             {
-                currencies = await apiFetcher.GetObjectAsync();
+                currencies = await apiFetcher.GetObjectAsync(); 
             });
         }
 
-
-        private void E_Focused(object sender, FocusEventArgs e)
+        private void Eur_Focused(object sender, FocusEventArgs e)
         {
-            if (currencies != null) DisplayAlert("init", "init " + currencies.Count.ToString(), "cancel");
-            //DisplayAlert("Focused", "Currencies ", "Cancel");
+            DrawChart(eur);
         }
 
-        private void E_TextChanged(object sender, TextChangedEventArgs e)
+        private void Eur_TextChanged(object sender, TextChangedEventArgs e)
         {
-            DisplayAlert("Changed", "Changed " + sender.ToString(), "Cancel");
+            throw new NotImplementedException();
+        }
+
+        private void Gbp_Focused(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Gbp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Uah_Focused(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Uah_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Rub_Focused(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Rub_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Ron_Focused(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Ron_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Usd_Focused(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Mdl_Focused(object sender, FocusEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Mdl_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Usd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
@@ -84,3 +166,15 @@ namespace Money
 //    e.TextChanged += E_TextChanged;
 //    e.Focused += E_Focused;
 //});
+
+//private void E_Focused(object sender, FocusEventArgs e)
+//{
+//    if (currencies != null) DisplayAlert("init", "init " + currencies.Count.ToString(), "cancel");
+//    //DisplayAlert("Focused", "Currencies ", "Cancel");
+//} 
+
+//private void E_TextChanged(object sender, TextChangedEventArgs e)
+//{
+//    DisplayAlert("Changed", "Changed " + sender.ToString(), "Cancel");
+//}
+
